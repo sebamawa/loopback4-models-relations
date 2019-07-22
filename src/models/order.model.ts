936a8a250e176c46152f6b986bdd1ddc2fee9501
sebamawa <sebamawa@hotmail.com>
2019-07-22 18:01:17 -0300

    update

--- src/models/order.model.ts
+++ src/models/order.model.ts
@@ -1,18 +1,25 @@
 import { Entity, model, property, belongsTo } from '@loopback/repository';
 import { Customer } from './customer.model';
 
-@model({
-  settings: {
-    "foreignKeys": {
-      "customerId": {
-        "name": "user_fkey",
-        "foreignKey": "id",
-        "entityKey": "id",
-        "entity": "Customer"
-      }
-    }
-  }
-})
+
+// POR AHORA LAS FK SE DEFINEN MANUALMENTE
+// La definicion de abajo genera conflicto al no reconocer la
+// columna customerId en la tabla order. Esta columna se genera
+// con el decorador @belongsTo()
+// (https://github.com/strongloop/loopback-next/issues/2766)
+// @model({
+//   settings: {
+//     "foreignKeys": {
+//       "fk_order_customerId": {
+//         "name": "fk_order_customerId",
+//         "foreignKey": "customerId",
+//         "entityKey": "id",
+//         "entity": "Customer"
+//       }
+//     }
+//   }
+// })
+@model()
 export class Order extends Entity {
   @property({
     type: 'number',
@@ -35,7 +42,9 @@ export class Order extends Entity {
   date: Date;
 
   // Each order belongs to a user, identified by its id (customerId)
-  @belongsTo(() => Customer)
+  @belongsTo(
+    () => Customer,
+  )
   customerId: number;
 
   @property({
